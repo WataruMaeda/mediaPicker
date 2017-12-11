d256a71641d6eac52a3c4ff0efc40628dd8c1c4b
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-12-11 15:04:27 -0800

    init project

---
+++ mediaPicker/MediaPicker.swift
@@ -0,0 +1,13 @@
+//
+//  MediaPicker.swift
+//  mediaPicker
+//
+//  Created by Wataru Maeda on 2017/12/11.
+//  Copyright © 2017 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class MediaPicker: NSObject {
+
+}
